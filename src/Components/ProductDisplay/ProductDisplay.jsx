e0cf61e4202d452459784a55764ad3c5a907d502
EmanTurk <143930452+EmanTurk@users.noreply.github.com>
2023-12-18 22:27:17 +0200

    product page is updated

---
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import './ProductDisplay.css'
+import star_icon from '../Assets/star_icon.png';
+import star_dull_icon from '../Assets/star_dull_icon.png';
+
+const ProductDisplay = (props) => {
+    const {product} = props;
+  return (
+    <div className='productdisplay'>
+<div className="productdisplay-left">
+<div className="productdiplay-img-list">
+    <img src={product.image} alt="" />
+    <img src={product.image} alt="" />
+    <img src={product.image} alt="" />
+    <img src={product.image} alt="" />
+</div>
+<div className="productdisplay-img">
+    <img className='productdisplay-main-img' src={product.image} alt="" />
+</div>
+
+</div>
+<div className="productdisplay-right">
+
+    <h1>{product.name}</h1>
+    <div className="productdisplay-right-star">
+        <img src={star_icon} alt="" />
+        <img src={star_icon} alt="" />
+        <img src={star_icon} alt="" />
+        <img src={star_icon} alt="" />
+        <img src={star_dull_icon} alt="" />
+        <p>(122)</p>
+    </div>
+    <div className="productdisplay-right-prices">
+        <div className="productdisplay-right-price-old">${product.old_price}</div>
+        <div className="productdisplay-right-price-new">${product.new}</div>
+        <div className="productdisplay-right-description">
+            A lightweight, made with 90% cotton. worn as undershirt or outer garmet.
+        </div>
+        <div className="productdisplay-right-size">
+            <h1>Select Size</h1>
+            <div className="productdisplay-right-size">
+                <div>S</div>
+                <div>M</div>
+                <div>L</div>
+                <div>XL</div>
+                <div>XXL</div>
+            </div>
+        </div>
+        <button>Add To Cart</button>
+        <p className='productdisplay-right-catagory'><span>Category :</span>Women, T-shirt, Crop Top</p>
+        <p className='productdisplay-right-catagory'><span>Tags :</span>Modern Style</p>
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default ProductDisplay
--- src/Pages/LoginSignUp.jsx
+++ src/Pages/LoginSignUp.jsx
@@ -1,26 +1,27 @@
-import React from 'react'
-import './CSS/LoginSignUp.css'
+import React from "react";
+import "./CSS/LoginSignUp.css";
 
 export const LoginSignUp = () => {
   return (
-    <div className='loginsignup'>
+    <div className="loginsignup">
       <div className="login-signup-container">
         <h1>Sign Up</h1>
         <div className="login-signup-fields">
-          <input type="text" placeholder='Your Name' />
-          <input type="email" placeholder='Email Address' />
-          <input type="password" placeholder='Password' />
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
         </div>
         <button>Continue</button>
-<p className="login-signup-login">Already Have An Account? <span>Login Here</span></p>     
-<div className="login-signup-agree">
-  <input type="checkbox" name='' id='' />
-  <p>By continuing I agree to the terms of use and privacy policy</p>
-</div>
- </div>
-
+        <p className="login-signup-login">
+          Already Have An Account? <span>Login Here</span>
+        </p>
+        <div className="login-signup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing I agree to the terms of use and privacy policy</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginSignUp
+export default LoginSignUp;
