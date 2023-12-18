c73ad6b08121a2ad9d81f3b7474a6bf351f8aff3
EmanTurk <143930452+EmanTurk@users.noreply.github.com>
2023-12-18 18:22:32 +0200

    Shop category for each category is added

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,27 @@ import Product from "./Pages/Product";
 import LoginSignUp from "./Pages/LoginSignUp";
 import Shop from "./Pages/Shop"; 
 import Cart from "./Pages/Cart"; 
+import Footer from "./Components/Footer/Footer";
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kid_banner from './Components/Assets/banner_kids.png'
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Shop />} />
-          <Route path="/men" element={<ShopCategory category='men'/>} />
-          <Route path="/women" element={<ShopCategory category='women'/>} />
-          <Route path="/kids" element={<ShopCategory category='kids'/>} />
+          <Route path="/" element={<Shop/>} />
+          <Route path="/men" element={<ShopCategory banner={men_banner} category='men'/>} />
+          <Route path="/women" element={<ShopCategory banner={women_banner} category='women'/>} />
+          <Route path="/kids" element={<ShopCategory banner={kid_banner} category='kid'/>} />
           <Route path="product" element={<Product/>}/>
           <Route path="/product/:productId" element={<Product />} />
           <Route path="/cart" element={<Cart/>}/>
           <Route path="/login" element={<LoginSignUp/>}/>
 
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
